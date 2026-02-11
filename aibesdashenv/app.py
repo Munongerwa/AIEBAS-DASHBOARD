d316c63f38037e7d68c6b01dffc58a1519c3f407
Munongerwa <batiraishemunongerwa960@gmail.com>
2026-02-11 14:39:00 +0200

    deploying to render update

--- aibesdashenv/app.py
+++ aibesdashenv/app.py
@@ -444,4 +444,5 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     
     # Run the app
-    app.run_server(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port, debug=False)
+    
